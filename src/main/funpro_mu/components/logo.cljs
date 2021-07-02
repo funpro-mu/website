67cea8f11fe27a39e4a72528609de5102a9674e4
Clarice Bouwer <cbillowes@gmail.com>
2021-07-02 18:41:08 +0400

    Increase the height of the petite logo

--- src/main/funpro_mu/components/logo.cljs
+++ src/main/funpro_mu/components/logo.cljs
@@ -6,4 +6,4 @@
    [:a {:href "#", :class "block"}
     [:span {:class "sr-only"} "funpro.mu"]
     [:img {:class "hidden md:block h-16" :src "img/funpro.mu-dark-theme.svg" :alt "Functional Programmers Community Logo" :title "funpro.mu Logo"}]
-    [:img {:class "block md:hidden h-8" :src "img/logo-petite-dark-theme.png" :alt "Logo"}]]])
+    [:img {:class "block md:hidden h-12" :src "img/logo-petite-dark-theme.png" :alt "Logo"}]]])
