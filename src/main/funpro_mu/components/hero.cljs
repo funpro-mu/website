83d4db42e3a786e832060c45856f97fc1f96130f
Marius A. Rabenarivo <mariusrabenarivo@gmail.com>
2021-07-04 18:14:05 +0400

    Add heading to hero image

--- src/main/funpro_mu/components/hero.cljs
+++ src/main/funpro_mu/components/hero.cljs
@@ -1,11 +1,8 @@
 (ns funpro-mu.components.hero)
 
 (defn hero []
-  [:div
-   [:div.absolute.inset-1-2
-    ;; We need the heading to have a background
-    ;; The heading must not stay on screen when scrolling
-    ;; The heading must be in the center and middle of the screen
-    ;; Animate the heading (like typing works etc)
-    [:h1.bg-black-pearl.text-9xl ""]]
+  [:div.relative.mt-16
+   [:div.max-w-screen-xl.mx-auto
+    [:div.absolute.top-1-2.transform.-translate-y-1-2
+      [:span.max-w-screen-xl.mx-auto.bg-black-pearl.bg-opacity-80.leading-snug.text-amber.text-9xl "(hello :world)"]]]
    [:img.w-screen.h-screen.object-cover {:src "img/hero.webp"}]])
