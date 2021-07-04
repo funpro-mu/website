aa1ecd0d007ba9476e8d409f890dfa516969d10b
Marius A. Rabenarivo <mariusrabenarivo@gmail.com>
2021-07-04 18:42:04 +0400

    Add typewriter animation to heading

--- src/main/funpro_mu/components/hero.cljs
+++ src/main/funpro_mu/components/hero.cljs
@@ -3,6 +3,6 @@
 (defn hero []
   [:div.relative.mt-16
    [:div.max-w-screen-xl.mx-auto
-    [:div.absolute.top-1-2.transform.-translate-y-1-2
-      [:span.max-w-screen-xl.mx-auto.bg-black-pearl.bg-opacity-80.leading-snug.text-amber.text-9xl "(hello :world)"]]]
+    [:div.absolute.top-1-2.transform.-translate-y-1-2.flex
+      [:span.typewriter.max-w-screen-xl.mx-auto.bg-black-pearl.bg-opacity-80.leading-snug.text-amber.text-9xl "(hello :world)"]]]
    [:img.w-screen.h-screen.object-cover {:src "img/hero.webp"}]])
