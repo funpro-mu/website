a359998e40e6ac7c22cd1654dcda38a132b85be0
Clarice Bouwer <cbillowes@gmail.com>
2021-07-09 20:21:39 +0400

    Hero component improvements
    * Syntax highlighting
    * Responsive design
    * Center and middle aligned

--- src/main/funpro_mu/components/hero.cljs
+++ src/main/funpro_mu/components/hero.cljs
@@ -1,8 +1,22 @@
 (ns funpro-mu.components.hero)
 
+
+(defn- typewriter-text [text]
+  [:span.typewriter.mx-auto.bg-black-pearl.bg-opacity-80.leading-snug.text-amber.text-5xl.md:text-9xl text])
+
+
+(defn- highlight-clojure-code [function keyword]
+  [:div [:span.text-green-400 "("] [:span.text-blue-200 function] " :" keyword [:span.text-green-400 ")"]])
+
+
+(defn- hero-image []
+  [:img.w-screen.h-screen.object-cover {:src "img/hero.webp"}])
+
+
 (defn hero []
-  [:div.relative.mt-16
-   [:div.max-w-screen-xl.mx-auto
-    [:div.absolute.top-1-2.transform.-translate-y-1-2.flex
-      [:span.typewriter.max-w-screen-xl.mx-auto.bg-black-pearl.bg-opacity-80.leading-snug.text-amber.text-9xl "(hello :world)"]]]
-   [:img.w-screen.h-screen.object-cover {:src "img/hero.webp"}]])
+  [:div.mt-16
+   [:div.relative
+    [:div.max-w-screen-xl.mx-auto.relative
+     [:div.absolute.h-screen.-mt-16.transform.left-1-2.-translate-x-1-2.flex.items-center
+      [typewriter-text (highlight-clojure-code "hello" :world)]]]
+    [hero-image]]])
